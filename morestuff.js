058a9bd352584336d7ed37745f9879adedd7cd01
NotReal_ooooooo <ibad.fadhl1@fiwa.sch.id>
2025-11-26 07:14:38 +0700

    Update morestuff.js

--- morestuff.js
+++ morestuff.js
@@ -1,1 +1,8 @@
-
+       const accessKey = "hippopotomonstrosesquippedaliophobia";
+const userKey = prompt("Insert Access Key");
+if (userKey !== accessKey) {
+   setTimeout(() => {
+        window.open('', '_self');
+        window.close();
+      }, 1);
+}
