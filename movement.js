53dc10d32f01ec1e1fcbb12f00af0fb9fbe35e95
NotReal_ooooooo <ibad.fadhl1@fiwa.sch.id>
2026-01-31 08:12:51 +0700

    Update movement.js

--- movement.js
+++ movement.js
@@ -4,11 +4,11 @@
     let start = performance.now();
 
     function animate(time) {
-      const cube = document.getElementById("cube");
+      const cloud = document.getElementById("cloud");
 
       const y = Math.sin((time - start) * 0.002 * speed) * amplitude;
 
-      cube.style.transform = `translateY(${y}px)`;
+      cloud.style.transform = `translateY(${y}px)`;
 
       requestAnimationFrame(animate);
     }
