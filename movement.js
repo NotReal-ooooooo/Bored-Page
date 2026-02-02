6610d1cc97966d48deda791e4e06a25136538cd8
NotReal_ooooooo <ibad.fadhl1@fiwa.sch.id>
2026-02-02 13:19:11 +0700

    Update movement.js

--- movement.js
+++ movement.js
@@ -1,5 +1,8 @@
  const amplitude = 10;
     let speed = 0.3;
+ const amplitude2 = 20;
+    let speed2 = 0.6;
+
 
     let start = performance.now();
 
@@ -8,10 +11,11 @@
       const beanimg = document.getElementById("beanimg");
 
       const y = Math.sin((time - start) * 0.002 * speed) * amplitude;
+      const yb = Math.sin((time - start) * 0.002 * speed2) * amplitude2;
 
       cloud.style.transform = `translateY(${y}px)`;
       beanimg.style.transform =
-  `translate(-50%, -50%) translateY(${y}px)`;
+  `translate(-50%, -50%) translateY(${yb}px)`;
 
 
       requestAnimationFrame(animate);
